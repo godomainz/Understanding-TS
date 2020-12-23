ee0854f01490dc6f796dc7ba90192d79cce77a3a
Akila Mendis <akila@oneit.com.au>
2020-12-23 11:27:22 +0530

    12. Working with Enums

--- app.ts
+++ app.ts
@@ -2,14 +2,33 @@
 //     name: "Akila",
 //     age: 30
 // }
-const person: {name:string, age:number,hobbies:string[],role:[number,string]} = {
+// const person: {name:string, age:number,hobbies:string[],role:[number,string]} = {
+//     name: "Akila",
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2,"author"]
+// }
+
+const  ADMIN = 0;
+// enum Role {
+//     ADMIN = 5, READ_ONLY, AUTHOR
+// }
+// enum Role {
+//     ADMIN = 5, READ_ONLY = 100, AUTHOR = 120
+// }
+enum Role {
+    ADMIN = "ADMIN", READ_ONLY = "READ_ONLY", AUTHOR = "AUTHOR"
+}
+
+const person = {
     name: "Akila",
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2,"author"]
+    role: Role.ADMIN
 }
 
-person.role[0]=5
-console.log(person.role[0])
-person.role.push("admin")
-console.log(person.role)
+console.log(person.role);
+console.log(Role.READ_ONLY);
+if (person.role === Role.ADMIN){
+    console.log("is admin");
+}
