85cc160a49b5c5bcf99e093a0df604407ffd4b9b
Akila Mendis <akila@oneit.com.au>
2020-12-23 15:37:44 +0530

    20. Functions as Types

---
+++ functions-as-types.ts
@@ -0,0 +1,14 @@
+export {};
+function add(n1: number, n2: number): number {
+    return n1 + n2;
+} 
+
+function printResult(num:number): void{
+    console.log("Result "+num);
+}
+printResult(add(5,6));
+
+let combineValues: (a: number, b: number) => number;
+combineValues = add;
+
+console.log(combineValues(8,8));
