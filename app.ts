68fc117e23c3a1f1936efaf163d6b9954099e2f1
Akila Mendis <akila@oneit.com.au>
2020-12-23 10:57:57 +0530

    11. Working with Tuples

--- app.ts
+++ app.ts
@@ -2,9 +2,14 @@
 //     name: "Akila",
 //     age: 30
 // }
-const person = {
+const person: {name:string, age:number,hobbies:string[],role:[number,string]} = {
     name: "Akila",
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2,"author"]
 }
-console.log(person.name)
-console.log(person.age)
+
+person.role[0]=5
+console.log(person.role[0])
+person.role.push("admin")
+console.log(person.role)
