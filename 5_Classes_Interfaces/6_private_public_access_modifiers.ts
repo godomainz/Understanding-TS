9e900df91c26ad29b5fd4103468d397f5fe20400
Akila Mendis <akila@oneit.com.au>
2020-12-24 16:45:17 +0530

    6. private and public Access Modifiers

---
+++ 5_Classes_Interfaces/6_private_public_access_modifiers.ts
@@ -0,0 +1,30 @@
+export {};
+class Department {
+    private  name: string;
+    private employees: string[] = [];
+
+    constructor(n:string){
+        this.name = n;
+    }
+
+    describe(){
+        console.log("Department: "+this.name);
+    }
+
+    addEmployee(employee:string){
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation(){
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+
+const accounting = new Department("Accounting");
+
+accounting.addEmployee("Akila");
+accounting.addEmployee("Nimal");
+accounting.describe();
+accounting.printEmployeeInformation();
+
